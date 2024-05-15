c18780ec7653eef0ade8428b4f1d628ef104ca1f
Mohammed-Rinshad <154004738+Mohammed-Rinshad@users.noreply.github.com>
2024-05-15 19:41:40 +0530

    Add files via upload

--- news.js
+++ news.js
@@ -4,14 +4,17 @@ let sports = document.getElementById('sports')
 let india = document.getElementById('india')
 let entertainment = document.getElementById('entertainment')
 let technology = document.getElementById('technology')
+let newsBody = document.getElementById('news-body')
+let newsImg = document.getElementById('news-img')
+let newsTitle = document.getElementById('h1-news')
 
 
 
-
-async function getnewsData(menuToshow){
+async function newsData(menuToshow){
     let currentDate = new Date();
-    let apiKey = '614e6a6713c54d94b449f5cffe92568b'
-    const url = `https://newsapi.org/v2/everything?q=${menuToshow}&from=${currentDate}&sortBy=publishedAt&apiKey=${apiKey}`
+    // let apiKey = 'd91c889a547145298f91beedaf7d0c03'
+    let apiKey2 = '614e6a6713c54d94b449f5cffe92568b'
+    const url = `https://newsapi.org/v2/everything?q=${menuToshow}&from=${currentDate}&sortBy=publishedAt&apiKey=${apiKey2}`
     let response = await fetch(url)
     let dataNews = await response.json()
     
@@ -95,12 +98,53 @@ async function getnewsData(menuToshow){
         india.style.color = "rgb(122, 121, 121)"
         // india.style.backgroundColor = "white"
     }
+
     // alert('function Ok')
-    for (let i = 0; i < dataNews.articles.length; i++) {
-    //console.log(dataNews.articles[i].title)
-    console.log(dataNews)
+    for (let i = 0; i < dataNews.articles.length ; i++) {
+    // console.log(dataNews.articles[i].title)
+    // console.log(dataNews.articles[i].urlToImage)
+    console.log(dataNews.articles);
+    
+    let newDiv = document.createElement('div')
+    newsBody.appendChild(newDiv)
+    newDiv.classList.add('news-body')
+
+    let createImg = document.createElement('img')
+    newsImg.appendChild(createImg)
+    createImg.classList.add('news-img')
+
+    let createTitle = document.createElement('h1')
+    newsTitle.appendChild(createTitle)
+    createTitle.classList.add('h1-news')
+
+    createTitle = dataNews.articles[i].title
+    newDiv.textContent = createTitle
+    createImg.src = dataNews.articles[i].urlToImage
+    newDiv.appendChild(createImg)
+
+    if(dataNews.articles[i].urlToImage === null && dataNews.articles[i].title !== "[Removed]"){
+        createImg.src = "https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"
+    }else if(dataNews.articles[i].title === "[Removed]"){
+        createImg.src = "https://filestore.community.support.microsoft.com/api/images/2f12ab59-a2a9-4f40-847f-76dc75093468?upload=true"
     }
+    
+    newDiv.addEventListener('click' ,() => {
+        window.open(dataNews.articles[i].url)
 
+    })
+    
+
+    }
+ 
 }
-// getnewsData()
+// newsData()
 
+    // let newNewsBody = document.createElement('div')
+    // newsBody.appendChild(newNewsBody)
+    // let newNewsTitle = document.createElement('h1')
+    // newNewsTitle.textContent = dataNews.articles[i].title
+    // newsTitle.appendChild(newNewsTitle)
+    // let newNewimg = document.createElement('img')
+    // newsImg.appendChild(newNewimg)
+    // newsImg.src = dataNews.articles[i].urlToImage
+    // console.log(dataNews)
